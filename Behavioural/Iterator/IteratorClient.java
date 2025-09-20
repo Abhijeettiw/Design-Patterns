30fd8cf2f5f981f1c85df534987163ed60078080
Abhijeet <abhijeet1992tiwari@gmail.com>
2025-09-20 22:10:52 +0530

    Iterator behavioral pattern

--- Behavioural/Iterator/IteratorClient.java
+++ Behavioural/Iterator/IteratorClient.java
@@ -1,6 +1,7 @@
 public class IteratorClient {
     public static void main(String[] args) {
         ConcreteIterator iterator = new ConcreteAggregate().getIterator();
+//        Iterator<Target> iterator = new ConcreteIterator();
         while (iterator.hasNext()){
             System.out.println(iterator.next());
         }
